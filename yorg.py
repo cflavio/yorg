c267a9bf1b5f82b657b812a4c6f47134cc7f2fad
Flavio Calva <flavio@ya2.it>
2015-06-18 08:39:02 +0200

    new music

--- yorg.py
+++ yorg.py
@@ -18,7 +18,7 @@ class _Audio(Audio):
     def __init__(self, mdt):
         Audio.__init__(self, mdt)
         self.menu_music = loader.loadSfx('assets/music/menu.ogg')
-        self.game_music = loader.loadSfx('assets/music/track.ogg')
+        self.game_music = loader.loadSfx('assets/music/on_the_other_side.ogg')
         map(lambda mus: mus.set_loop(True), [self.menu_music, self.game_music])
 
 
