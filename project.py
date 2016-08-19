3e86887a2df563c686128dc6eeb819e8f91341ac
Flavio Calva <flavio@ya2.it>
2016-08-19 18:44:51 +0200

    project file

---
+++ project.py
@@ -0,0 +1,10 @@
+#TODO: (1.9.1) remove request-dxdisplay-information for detecting the resolution on Windows
+#TODO: embed into a wx window
+#TODO: autoinstalling panda plugin http://www.panda3d.org/forums/viewtopic.php?f=1&t=16321
+#TODO: do an abstract class Game with virtual methods drawMenuBackground, onPlay, onLoad
+#TODO: pipelined rendering loadPrcFileData('', 'threading-model /Draw')
+#TODO: pause: put all tasks on a taskchain and deactivate it
+#TODO: open the browser at the end
+#TODO: build only nointernet, linux64, win64, osx64, without runtime caching, runtime-dev option
+#TODO: branches master and stable (not dev)
+#TODO: sharing times on social network
