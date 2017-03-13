132a7acb328167bea78629463010d5aa75f9c8b7
Flavio Calva <flavio@ya2.it>
2017-03-13 19:44:21 +0100

    refactoring

--- yorg/audio.py
+++ yorg/audio.py
@@ -5,5 +5,6 @@ class _Audio(Audio):
 
     def __init__(self, mdt):
         Audio.__init__(self, mdt)
-        self.menu_music = loader.loadSfx('assets/music/Start Your Engines.ogg')
+        bg_menu = 'assets/music/Inizio - Start Your Engines.ogg'
+        self.menu_music = loader.loadSfx(bg_menu)
         self.menu_music.set_loop(True)
