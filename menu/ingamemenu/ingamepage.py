a4015e87f7f2781b8de39137f8dc8855bec2a170
Flavio Calva <flavio@ya2.it>
2017-11-29 00:14:20 +0100

    fixes for 1.9.2

--- menu/ingamemenu/ingamepage.py
+++ menu/ingamemenu/ingamepage.py
@@ -35,7 +35,11 @@ class InGamePageGui(PageGui):
             text_scale=.8, **btn_args)
         map(self.add_widget, [frm, txt, btn_visit, btn_dont_visit])
         PageGui.bld_page(self, False)
-        self.eng.show_cursor()
+
+        #self.eng.show_cursor()  # doesn't work on 1.9.2
+        self.eng.hide_cursor()
+        self.eng.show_standard_cursor()
+
         self.eng.do_later(.01, self.eng.toggle_pause, [False])
         # in the next frame since otherwise InGameMenu will be paused while
         # waiting page's creation, and when it is restored it is destroyed,
