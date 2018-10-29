ef8880b3181603dd67efe358e16e4e03b650c293
Flavio Calva <flavio@ya2.it>
2018-10-29 22:16:55 +0100

    refactoring (gfx)

--- project.py
+++ project.py
@@ -7,16 +7,13 @@
 # improve sfx
 # dust
 # explosion
-# weapon effects
+# trail effect with glow for weapons
 
 
 # WAITING =====
 
 # (Panda3D 1.10) joypad
 # (Panda3D 1.10) remove thirdparty libraries (manage them with deploy-ng)
-# (Panda3D 1.10) write snow shader
-# (Panda3D 1.10) hw skinning
-# (Panda3D 1.10) deployng: log_filename
 # (Panda3D 1.10) deployng: use logging for logging
 # (Panda3D 1.10) port to python 3
 # (Panda3D 1.10) fix curr_ver == 'deploy-ng' in engine.logic
@@ -27,12 +24,6 @@
 
 # make scons for yyagl
 # make a submodule for racing - yyarl
-# embed into a wx / pyqt window
-# trail effect with glow for weapons
-# uml create automatic class diagrams with fields and methods for each class
-# do automatic update (assets shared among platforms)
-# unit tests
-# django webapp for scores
 # refactor: use only eng.client (remove eng.server and yorg_client)
 # refactor: don't share eng with every colleague, instead share only the
 #   useful components e.g. PhysComponent has PhysComponent.phys_mgr and
@@ -40,9 +31,10 @@
 # refactor: remove mediator from colleague
 # refactor: where proper (i.e. where observers aren't tied to the observable)
 #   replace observer with publisher-subscriber
-# add car helios
-# add friendship
 # drifting force function of linear and angular velocities
-# do a single page with track, drivers, cars and messages for all: single,
-#   local and online multiplayer
-# refactoring: select the driver before the car
+# uml create automatic class diagrams with fields and methods for each class
+# unit tests
+# embed into a wx / pyqt window
+# do automatic update (assets shared among platforms)
+# add friendship
+# django webapp for scores
