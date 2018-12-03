b2084bcfa62e601dd0b95ee9cb412b77b3e99cc7
Flavio Calva <flavio@ya2.it>
2018-12-03 23:36:43 +0100

    refactoring of facade

--- project.py
+++ project.py
@@ -19,6 +19,8 @@
 # (Panda3D 1.10) fix curr_ver == 'deploy-ng' in engine.logic
 # (fixed is_in_contact) if not is_in_contact: horizontal ai rays, not inclined
 #   like the car (issue 23)
+# (refactored objects' creation) refactor: facade, pass a single list (meth for
+#   for callables, prop for others)
 
 
 # MAYBE/SOMEDAY =====
@@ -28,9 +30,10 @@
 #   replace observer with publisher-subscriber
 # refactor: attach/attach_obs, detach/detach_obs - the client attach-es it to
 #   the observed, then it attach-es it to the component
-# refactor: Facade.__init__(self, mth_lst, prop_lst), internally it invokes
-#   the methods _fwd_mth, _fwd_prop
 # refactor: notify's sender (see page.py)
+# refactor: object's creation: isolate the parallel creation and construct
+#   object in the standard way (fields) and use the parallel creation only when
+#   it is useful
 # refactor: racing should be another package in another submodule (i.e. yorg
 #   contains yyagl/ and racing/)
 # make scons for yyagl
