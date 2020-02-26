2f82a9b81b062db173b3a8a885cc75c4e6dd0af7
Flavio Calva <flavio@ya2.it>
2020-02-26 19:42:57 +0100

    setuptools: source building

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from os import system
 from setuptools import setup
 from setuptools.command.develop import develop
+from distutils.cmd import Command
+from distutils.log import INFO
+from yyagl.build.src import bld_src
 
 
 class DevelopPyCommand(develop):
@@ -10,11 +13,28 @@ class DevelopPyCommand(develop):
         system('scons lang=1 images=1 tracks=1')
 
 
+class SourcePkgCommand(Command):
+
+    user_options = []
+
+    def initialize_options(self): pass
+
+    def finalize_options(self): pass
+
+    def run(self):
+        self.announce(
+            'Building source package',
+            level=INFO)
+        bld_src(None, None, {'APPNAME': 'yorg'})
+
+
 if __name__ == '__main__':
     setup(
         name='Yorg',
         version=0.9,
-        cmdclass={'develop': DevelopPyCommand},
+        cmdclass={
+            'develop': DevelopPyCommand,
+            'source_pkg': SourcePkgCommand},
         install_requires=[
             'SCons==2.5.0',
             # 'panda3d'  # it doesn't pull the dependency
