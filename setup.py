99fb94ef90d5a01f59fd3a47e80c7a6e272b55cf
Flavio Calva <flavio@ya2.it>
2020-03-04 00:09:33 +0100

    setuptools: builds

--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ from yyagl.build.uml import bld_uml
 from yyagl.build.imgs import bld_images
 from yyagl.build.tracks import bld_models
 from yyagl.build.strings import bld_mo, bld_pot, bld_merge
+from yyagl.build.linux import bld_linux
+from yyagl.build.windows import bld_windows
+from yyagl.build.appimage import bld_appimage
+from yyagl.build.flatpak import bld_flatpak
 
 
 class DevelopPyCmd(develop):
@@ -87,9 +91,13 @@ class PDFCmd(AbsCmd):
 
 class ImagesCmd(AbsCmd):
 
+    already_run = False
+
     def run(self):
+        if ImagesCmd.already_run: return
         bld_images(
             None, files(['jpg', 'png'], ['tex'], ['_png.png']), AbsCmd.env)
+        ImagesCmd.already_run = True
 
 
 class UMLCmd(AbsCmd):
@@ -101,16 +109,21 @@ class UMLCmd(AbsCmd):
 
 class ModelsCmd(AbsCmd):
 
+    already_run = False
+
     def run(self):
+        if ModelsCmd.already_run: return
         AbsCmd.env['MODELS_DIR_PATH'] = 'assets/models'
         AbsCmd.env['CARS_DIR_PATH'] = 'assets/cars'
         AbsCmd.env['TRACKS_DIR_PATH'] = 'assets/tracks'
         bld_models(None, None, AbsCmd.env)
+        ModelsCmd.already_run = True
 
 
 class LangCmd(AbsCmd):
 
     lang_path = 'assets/locale/'
+    already_run = False
 
     def _process_lang(self, lang_code):
         lang_name = 'assets/po/%s.po' % lang_code
@@ -119,9 +132,51 @@ class LangCmd(AbsCmd):
         bld_mo(lang_mo, None, AbsCmd.env)
 
     def run(self):
+        if LangCmd.already_run: return
         AbsCmd.env['LNG'] = self.lang_path
         bld_pot(None, None, AbsCmd.env)
         list(map(self._process_lang, ['it_IT', 'de_DE', 'gd', 'es_ES', 'gl_ES', 'fr_FR']))
+        LangCmd.already_run = True
+
+
+class LinuxCmd(AbsCmd):
+
+    def run(self):
+        self.run_command('images')
+        self.run_command('models')
+        self.run_command('lang')
+        AbsCmd.env['ICO_FPATH'] = 'assets/images/icon/icon%s_png.png'
+        bld_linux(None, None, AbsCmd.env)
+
+
+class WindowsCmd(AbsCmd):
+
+    def run(self):
+        self.run_command('images')
+        self.run_command('models')
+        self.run_command('lang')
+        bld_windows(None, None, AbsCmd.env)
+
+
+class AppImageCmd(AbsCmd):
+
+    def run(self):
+        self.run_command('images')
+        self.run_command('models')
+        self.run_command('lang')
+        AbsCmd.env['ICO_FPATH'] = 'assets/images/icon/icon%s_png.png'
+        bld_appimage(None, None, AbsCmd.env)
+
+
+class FlatPakCmd(AbsCmd):
+
+    def run(self):
+        #self.run_command('images')
+        #self.run_command('models')
+        #self.run_command('lang')
+        AbsCmd.env['ICO_FPATH'] = 'assets/images/icon/icon%s_png.png'
+        AbsCmd.env['FLATPAK_DST'] = '.'
+        bld_flatpak(None, None, AbsCmd.env)
 
 
 if __name__ == '__main__':
@@ -137,7 +192,11 @@ if __name__ == '__main__':
             'uml': UMLCmd,
             'images': ImagesCmd,
             'models': ModelsCmd,
-            'lang': LangCmd},
+            'lang': LangCmd,
+            'linux': LinuxCmd,
+            'windows': WindowsCmd,
+            'appimage': AppImageCmd,
+            'flatpak': FlatPakCmd},
         install_requires=[
             'SCons==2.5.0',
             # 'panda3d'  # it doesn't pull the dependency
