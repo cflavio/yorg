d16c92000fbfc3602e2c09165161e1231ad13e7b
Flavio Calva <flavio@ya2.it>
2020-03-04 23:21:49 +0100

    setuptools: bdist_apps

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from os import system, getcwd
+from os.path import exists
 from collections import namedtuple
 from setuptools import setup
 from setuptools.command.develop import develop
 from distutils.cmd import Command
-from yyagl.build.build import files
+from direct.dist.commands import bdist_apps
+from shutil import move, rmtree
+from yyagl.build.build import bld_dpath, branch, files
 from yyagl.build.src import bld_src
 from yyagl.build.devinfo import bld_devinfo
 from yyagl.build.docs import bld_docs
@@ -16,6 +19,10 @@ from yyagl.build.linux import bld_linux
 from yyagl.build.windows import bld_windows
 from yyagl.build.appimage import bld_appimage
 from yyagl.build.flatpak import bld_flatpak
+
+
+msg = '''NOTE: please be sure that you've already created the assets with:
+    * python setup.py images models lang'''
 
 
 class DevelopPyCmd(develop):
@@ -91,13 +98,9 @@ class PDFCmd(AbsCmd):
 
 class ImagesCmd(AbsCmd):
 
-    already_run = False
-
     def run(self):
-        if ImagesCmd.already_run: return
         bld_images(
             None, files(['jpg', 'png'], ['tex'], ['_png.png']), AbsCmd.env)
-        ImagesCmd.already_run = True
 
 
 class UMLCmd(AbsCmd):
@@ -109,21 +112,16 @@ class UMLCmd(AbsCmd):
 
 class ModelsCmd(AbsCmd):
 
-    already_run = False
-
     def run(self):
-        if ModelsCmd.already_run: return
         AbsCmd.env['MODELS_DIR_PATH'] = 'assets/models'
         AbsCmd.env['CARS_DIR_PATH'] = 'assets/cars'
         AbsCmd.env['TRACKS_DIR_PATH'] = 'assets/tracks'
         bld_models(None, None, AbsCmd.env)
-        ModelsCmd.already_run = True
 
 
 class LangCmd(AbsCmd):
 
     lang_path = 'assets/locale/'
-    already_run = False
 
     def _process_lang(self, lang_code):
         lang_name = 'assets/po/%s.po' % lang_code
@@ -132,38 +130,15 @@ class LangCmd(AbsCmd):
         bld_mo(lang_mo, None, AbsCmd.env)
 
     def run(self):
-        if LangCmd.already_run: return
         AbsCmd.env['LNG'] = self.lang_path
         bld_pot(None, None, AbsCmd.env)
         list(map(self._process_lang, ['it_IT', 'de_DE', 'gd', 'es_ES', 'gl_ES', 'fr_FR']))
-        LangCmd.already_run = True
-
-
-class LinuxCmd(AbsCmd):
-
-    def run(self):
-        self.run_command('images')
-        self.run_command('models')
-        self.run_command('lang')
-        AbsCmd.env['ICO_FPATH'] = 'assets/images/icon/icon%s_png.png'
-        bld_linux(None, None, AbsCmd.env)
-
-
-class WindowsCmd(AbsCmd):
-
-    def run(self):
-        self.run_command('images')
-        self.run_command('models')
-        self.run_command('lang')
-        bld_windows(None, None, AbsCmd.env)
 
 
 class AppImageCmd(AbsCmd):
 
     def run(self):
-        self.run_command('images')
-        self.run_command('models')
-        self.run_command('lang')
+        print(msg)
         AbsCmd.env['ICO_FPATH'] = 'assets/images/icon/icon%s_png.png'
         bld_appimage(None, None, AbsCmd.env)
 
@@ -171,18 +146,28 @@ class AppImageCmd(AbsCmd):
 class FlatPakCmd(AbsCmd):
 
     def run(self):
-        #self.run_command('images')
-        #self.run_command('models')
-        #self.run_command('lang')
+        print(msg)
         AbsCmd.env['ICO_FPATH'] = 'assets/images/icon/icon%s_png.png'
         AbsCmd.env['FLATPAK_DST'] = '.'
         bld_flatpak(None, None, AbsCmd.env)
 
 
+class BDistAppsCmd(bdist_apps):
+
+    def run(self):
+        print(msg)
+        bdist_apps.run(self)
+        bld_windows(None, None, AbsCmd.env)
+        AbsCmd.env['ICO_FPATH'] = 'assets/images/icon/icon%s_png.png'
+        bld_linux(None, None, AbsCmd.env)
+        rmtree('build')
+        rmtree('dist')
+
+
 if __name__ == '__main__':
     setup(
-        name='Yorg',
-        version=0.9,
+        name=AbsCmd.env['APPNAME'],
+        version=branch,
         cmdclass={
             'develop': DevelopPyCmd,
             'source_pkg': SourcePkgCmd,
@@ -193,11 +178,49 @@ if __name__ == '__main__':
             'images': ImagesCmd,
             'models': ModelsCmd,
             'lang': LangCmd,
-            'linux': LinuxCmd,
-            'windows': WindowsCmd,
             'appimage': AppImageCmd,
-            'flatpak': FlatPakCmd},
+            'flatpak': FlatPakCmd,
+            'bdist_apps': BDistAppsCmd},
         install_requires=[
-            'SCons==2.5.0',
+            #'SCons==2.5.0',
             # 'panda3d'  # it doesn't pull the dependency
-            ])
+            ],
+        options = {
+            'build_apps': {
+                'exclude_patterns': [
+                    'build/*', 'built/*', 'setup.py', 'requirements.txt', '*.swp',
+                    'SConstruct', 'venv/*', '.git*', '*.pyc', 'options.json',
+                    '__pycache__'],
+                'log_filename': '$USER_APPDATA/Yorg/p3d_log.log',
+                'plugins': ['pandagl', 'p3openal_audio'],
+                'gui_apps': {'appname': 'main.py'},
+                'icons': {
+                    'appname': [
+                        'assets/images/icon/icon256_png.png', 'assets/images/icon/icon128_png.png',
+                        'assets/images/icon/icon48_png.png', 'assets/images/icon/icon32_png.png',
+                        'assets/images/icon/icon16_png.png']},
+                'include_patterns': [
+                    '**/yyagl/licenses/*',
+                    '**/licenses/*',
+                    '**/*.bam',
+                        '**/*.txo',
+                        '**/*.json',
+                        '**/track_tr.py',
+                        '**/*.txt',
+                        '**/*.ttf',
+                        '**/*.vert',
+                        '**/*.frag',
+                        '**/*.ogg',
+                        '**/*.wav',
+                        '**/*.mo'],
+                    'platforms': [
+                        'win_amd64',
+                        'manylinux1_x86_64'
+                    ],
+                    'include_modules': {'*': ['encodings.hex_codec']}},
+            'bdist_apps': {
+                'installers': {
+                    'manylinux1_x86_64': ['xztar'],
+                    'win_amd64': ['xztar', 'nsis']
+                }}}
+    )
