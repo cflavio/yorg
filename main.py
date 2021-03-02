85a85d1a224b2393c812f109e81b6ccca47e5088
Flavio Calva <flavio@ya2.it>
2021-03-02 08:39:22 +0100

    fixed logging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os.path import exists
+from logging import info, getLogger
 from traceback import print_exc
 # import direct.particles.ParticleManagerGlobal  # for deploy-ng
 from yorg.yorg import Yorg
@@ -9,4 +10,8 @@ if __name__ == '__main__' or exists('main.pyo'):
     try: yorg.run()
     except Exception as exc:
         print_exc()
+        logger = getLogger()
+        info(str(exc))
+        logger.handlers[0].flush()
         yorg.kill()
+        raise exc
